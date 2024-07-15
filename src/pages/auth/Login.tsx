bc4f3c904cb794882f8eefcf1edaae8c3e7a03e1
Mauricio Aguilera <mauricio@familify.com>
2024-07-14 20:20:00 -0500

    login pages

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,47 @@
+import { Box, Container } from '@mui/material';
+import React from 'react';
+import backgroundImage from '../../assets/bg_auth.jpg';
+import LoginForm from '../../components/forms/auth/LoginForm';
+
+
+
+const Login: React.FC = () => {
+
+  return (
+    <Box
+      id="box-1"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        minWidth: '100vw',
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        margin: 0,
+        padding: 0,
+      }}
+    >
+      <Container
+        id="container-1"
+        component="main"
+        maxWidth="xs"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(255, 255, 255, 0.85)',
+          borderRadius: 1,
+          boxShadow: 3,
+          padding: 4,
+        }}
+      >
+        <LoginForm />
+      </Container>
+    </Box>
+  );
+}
+
+export default Login;
