241900b8bb16c2b6919c193e49a06d99d383e5a8
Mauricio Aguilera <mauricio@familify.com>
2024-07-14 20:19:46 -0500

    login form

---
+++ src/components/forms/auth/LoginForm.tsx
@@ -0,0 +1,78 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthService from '../../../services/AuthService';
+import useAuthStore from '../../../stores/useAuthStore';
+
+
+const LoginForm: React.FC = () => {
+  const navigate = useNavigate();
+  const setToken = useAuthStore((state) => state.setToken);
+  const setUser = useAuthStore((state) => state.setUser);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const token = await AuthService.getToken({ email, password });
+    if (token?.access) {
+      setToken(token);
+      const fetchedUser = await AuthService.fetchUser(token.access);
+      if (fetchedUser) {
+        console.log(fetchedUser);
+        setUser(fetchedUser);
+        navigate('/dashboard');
+      } else {
+        alert('Error al obtener el usuario');
+      }
+    } else {
+      alert('Error en el inicio de sesión');
+    }
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.2 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 2 }}
+      style={{ width: '100%' }}
+    >
+
+      <Box component="form" onSubmit={handleLogin} sx={{ width: '100%' }}>
+        <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
+          Sign in
+        </Typography>
+        <TextField
+          id="email"
+          label="Email Address"
+          name="email"
+          margin="normal"
+          autoComplete="email"
+          required
+          fullWidth
+          autoFocus
+          value={email}
+          onChange={(e: any) => setEmail(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e: any) => setPassword(e.target.value)}
+        />
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          Sign In
+        </Button>
+      </Box>
+    </motion.div>
+  );
+};
+
+export default LoginForm;
