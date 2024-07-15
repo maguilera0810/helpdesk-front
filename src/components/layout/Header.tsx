da51cb94a5697ced179440512da65a538f43461f
Mauricio Aguilera <mauricio@familify.com>
2024-07-15 00:16:33 -0500

    header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,11 @@
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuthStore from '../../stores/useAuthStore';
+import useUIStore from '../../stores/useUIStore';
 
 interface HeaderProps {
   handleDrawerToggle: () => void;
@@ -13,6 +15,8 @@ const Header: React.FC<HeaderProps> = ({ handleDrawerToggle }) => {
   const navigate = useNavigate();
   const user = useAuthStore((state) => state.user);
   const logout = useAuthStore((state) => state.logout);
+  const isDrawerOpen = useUIStore((state) => state.isDrawerOpen);
+  const toggleDrawer = useUIStore((state) => state.toggleDrawer);
 
   const handleLogout = () => {
     logout();
@@ -26,10 +30,10 @@ const Header: React.FC<HeaderProps> = ({ handleDrawerToggle }) => {
           color="inherit"
           aria-label="open drawer"
           edge="start"
-          onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: 'none' } }}
+          onClick={toggleDrawer}
+          sx={{ mr: 2 }}
         >
-          <MenuIcon />
+          {isDrawerOpen ? <MenuOpenIcon /> : <MenuIcon />}
         </IconButton>
         <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
           MyApp
