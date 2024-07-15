c37f2f93cdc50d15af365070c5ad55b46dcffdb7
Mauricio Aguilera <mauricio@familify.com>
2024-07-14 20:56:21 -0500

    make protected routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,43 @@
 // src/routes.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
-import Login from './pages/auth/Login';
 import Dashboard from './components/Dashboard';
+import Login from './pages/auth/Login';
+
+import useAuthStore from './stores/useAuthStore';
+
+
+const PublicRoute: React.FC = () => {
+  const token = useAuthStore((state) => state.token);
+
+  return token ? <Navigate to="/dashboard" /> : <Outlet />;
+};
+
+
+
+
+const ProtectedRoute: React.FC = () => {
+  const token = useAuthStore((state) => state.token);
+
+  return token ? <Outlet /> : <Navigate to="/auth/login" />;
+};
+
+
+
 
 const AppRoutes: React.FC = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/auth/login" element={<Login />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<App />} />
+      </Route>
+      <Route element={<PublicRoute />}>
+        <Route path="/auth/login" element={<Login />} />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   </Router>
 );
