e59f0e5c928bf3e13c38225f9e917f5453174f06
Mauricio Aguilera <mauricio@familify.com>
2024-07-15 23:51:11 -0500

    List Users pages

---
+++ src/pages/admin/ListUsers.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+import Layout from '../../components/layout/Layout';
+import UserTable from '../../components/tables/UserTable';
+
+const Users: React.FC = () => {
+  return (
+    <Layout>
+      <Box sx={{ padding: 2 }}>
+        <Typography variant="h4">
+          Listado de Usuarios
+        </Typography>
+        <UserTable />
+      </Box>
+    </Layout>
+  );
+};
+
+export default Users;
