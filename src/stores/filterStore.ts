791f02aafd70aaa9d06de109e9b6d9a2a6ed99ef
Mauricio Aguilera <mauricio@familify.com>
2024-07-17 00:25:28 -0500

    filterStore

---
+++ src/stores/filterStore.ts
@@ -0,0 +1,24 @@
+import { produce } from 'immer';
+import { create, StateCreator } from 'zustand';
+
+
+interface IFilterState {
+  filters: { [key: string]: any };
+  setFilters: (filters: { [key: string]: any }) => void;
+  clearFilters: () => void;
+}
+
+const stateCreator: StateCreator<IFilterState, [], [], IFilterState> = (set) => ({
+  filters: {},
+  setFilters: (filters) =>
+    set(produce((state: IFilterState) => {
+      state.filters = filters
+    })),
+  clearFilters: () =>
+    set(produce((state: IFilterState) => {
+      state.filters = {}
+    })),
+});
+
+const filterStore = create<IFilterState>()(stateCreator);
+export default filterStore;
