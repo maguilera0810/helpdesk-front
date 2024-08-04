67ce6303635b8761ccbb2f2f300d1ce3b9b07dc6
Mauricio Aguilera <mauricio@familify.com>
2024-08-04 11:33:31 -0500

    model interface

---
+++ src/interfaces/ModelInterfaces.ts
@@ -0,0 +1,21 @@
+// AUTHENTICATION
+
+
+export interface UserProfile {
+  document: string;
+  documentType: string;
+  phone: string;
+  address: string;
+  isAvailable: boolean;
+}
+
+export interface User {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  profile: Partial<UserProfile>;
+  password?: string;
+}
+
