32b35f38d004d87eef08536a20942f4bdced89f0
Mauricio Aguilera <mauricio@familify.com>
2024-08-04 11:33:47 -0500

    auth interfaces

--- src/interfaces/AuthInterfaces.ts
+++ src/interfaces/AuthInterfaces.ts
@@ -16,32 +16,6 @@ export interface User {
   password?: string;
 }
 
-
-export interface User2 {
-  id: number;
-  username: string; // cedula
-  firstName: string;
-  lastName: string;
-  email: string;
-  password?: string;
-  cellphone?: string;
-}
-
-export interface Role {
-  id: number;
-  name: string;
-  description: string;
-  settings: object;
-  menu: object;
-}
-
-export interface Setting {
-  id: number;
-  name: string;
-  description: string;
-  is_active: boolean
-}
-
 export interface Token {
   refresh?: string;
   access?: string;
