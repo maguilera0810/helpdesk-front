8eef0746da6c2e47d7e92c7411b90725ca71276c
Mauricio Aguilera <mauricio@familify.com>
2024-08-04 10:19:45 -0500

    user service

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,15 +1,14 @@
-import Environment from '../environments/Environment';
 import { User } from '../interfaces/AuthInterfaces';
-import { keysToCamel, keysToSnake } from '../utils/caseUtils';
-import BaseService from './BaseService';
+import BaseCrudService from './BaseCrudService'; // TODO VERIFICAR POR QUE NO FUNCIONA??
 
-const { apiUrl } = Environment;
 
-export class UserService extends BaseService {
+
+
+export class UserService extends BaseCrudService<User> {
   private static instance: UserService;
 
   private constructor() {
-    super(`${apiUrl}/api/auth/admin-user/`);
+    super(`/api/auth/admin-user/`);
   }
 
   public static getInstance(): UserService {
@@ -19,60 +18,61 @@ export class UserService extends BaseService {
     return UserService.instance;
   }
 
-  public async list(filters: { [key: string]: any } = {}): Promise<User[]> {
-    try {
-      const response = await this.axiosInstance.get<User[]>('', {
-        headers: this.getAuthHeaders(),
-        params: keysToSnake(filters),
-      });
-      return keysToCamel(response.data);
-    } catch (error) {
-      console.error('SERVICE: Error fetching users:', error);
-      throw error;
-    }
-  }
+  // TODO BORRAR CUANDO SE VERIFIQUE QUE FUNCIONEN LAS CLASES PADRE
+  // public async list(filters: { [key: string]: any } = {}): Promise<User[]> {
+  //   try {
+  //     const response = await this.axiosInstance.get<User[]>('', {
+  //       headers: this.getAuthHeaders(),
+  //       params: keysToSnake(filters),
+  //     });
+  //     return keysToCamel(response.data);
+  //   } catch (error) {
+  //     console.error('SERVICE: Error fetching users:', error);
+  //     throw error;
+  //   }
+  // }
 
-  public async retrieve(userId: number): Promise<User> {
-    try {
-      const response = await this.axiosInstance.get<User>(`${userId}`,
-        { headers: this.getAuthHeaders() });
-      return keysToCamel(response.data);
-    } catch (error) {
-      console.error(`SERVICE: Error fetching user with id ${userId}:`, error);
-      throw error;
-    }
-  }
+  // public async retrieve(userId: number): Promise<User> {
+  //   try {
+  //     const response = await this.axiosInstance.get<User>(`${userId}`,
+  //       { headers: this.getAuthHeaders() });
+  //     return keysToCamel(response.data);
+  //   } catch (error) {
+  //     console.error(`SERVICE: Error fetching user with id ${userId}:`, error);
+  //     throw error;
+  //   }
+  // }
 
-  public async create(user: Partial<User>): Promise<User> {
-    try {
-      const response = await this.axiosInstance.post<User>('', keysToSnake(user),
-        { headers: this.getAuthHeaders() });
-      return keysToCamel(response.data);
-    } catch (error) {
-      console.error('SERVICE: Error creating user:', error);
-      throw error;
-    }
-  }
+  // public async create(user: Partial<User>): Promise<User> {
+  //   try {
+  //     const response = await this.axiosInstance.post<User>('', keysToSnake(user),
+  //       { headers: this.getAuthHeaders() });
+  //     return keysToCamel(response.data);
+  //   } catch (error) {
+  //     console.error('SERVICE: Error creating user:', error);
+  //     throw error;
+  //   }
+  // }
 
-  public async update(userId: number, user: Partial<User>): Promise<User> {
-    try {
-      const response = await this.axiosInstance.put<User>(`${userId}`, keysToSnake(user),
-        { headers: this.getAuthHeaders() });
-      return keysToCamel(response.data);
-    } catch (error) {
-      console.error(`SERVICE: Error updating user with id ${userId}:`, error);
-      throw error;
-    }
-  }
+  // public async update(userId: number, user: Partial<User>): Promise<User> {
+  //   try {
+  //     const response = await this.axiosInstance.put<User>(`${userId}`, keysToSnake(user),
+  //       { headers: this.getAuthHeaders() });
+  //     return keysToCamel(response.data);
+  //   } catch (error) {
+  //     console.error(`SERVICE: Error updating user with id ${userId}:`, error);
+  //     throw error;
+  //   }
+  // }
 
-  public async destroy(userId: number): Promise<void> {
-    try {
-      await this.axiosInstance.delete(`${userId}`, { headers: this.getAuthHeaders() });
-    } catch (error) {
-      console.error(`SERVICE: Error deleting user with id ${userId}:`, error);
-      throw error;
-    }
-  }
+  // public async destroy(userId: number): Promise<void> {
+  //   try {
+  //     await this.axiosInstance.delete(`${userId}`, { headers: this.getAuthHeaders() });
+  //   } catch (error) {
+  //     console.error(`SERVICE: Error deleting user with id ${userId}:`, error);
+  //     throw error;
+  //   }
+  // }
 }
 
 export default UserService.getInstance();
