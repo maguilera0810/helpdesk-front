86d923a74db7a19b7131cf80a3aa558f927f15e3
Mauricio Aguilera <mauricio@familify.com>
2024-08-18 15:33:26 -0500

    add date provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
 import './index.css';
 import AppRoutes from './routes';
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RecoilRoot>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <AppRoutes />
-    </RecoilRoot>
+    </LocalizationProvider>
   </React.StrictMode>,
 )
