b323521b2b973f88cd98e4b074ae7251eb7356e8
Mauricio Aguilera <mauricio@familify.com>
2024-08-24 00:53:41 -0500

    category detail page

---
+++ src/pages/admin/category/CategoryDetail.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Box, CssBaseline } from '@mui/material';
+import CategoryForm from '../../../components/forms/admin/category/CategoryForm';
+import Layout from '../../../components/layouts/Layout';
+
+const CategoryDetail: React.FC = () => {
+  return (
+    <Layout>
+      <Box
+        sx={{
+          marginTop: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <CssBaseline />
+        <CategoryForm />
+      </Box>
+    </Layout>
+  );
+};
+
+
+export default CategoryDetail;
