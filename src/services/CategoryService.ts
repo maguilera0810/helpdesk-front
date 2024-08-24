9c515097b163a1111b5a6e1e1e9adbd0d96e69e0
Mauricio Aguilera <mauricio@familify.com>
2024-08-24 00:53:20 -0500

    category service

---
+++ src/services/CategoryService.ts
@@ -0,0 +1,20 @@
+import { Category } from '../interfaces/ModelInterfaces';
+import BaseCrudService from './BaseCrudService';
+
+export class CategoryService extends BaseCrudService<Category> {
+  private static instance: CategoryService;
+
+  private constructor() {
+    super(`/api/common/category/`);
+  }
+
+  public static getInstance(): CategoryService {
+    if (!CategoryService.instance) {
+      CategoryService.instance = new CategoryService();
+    }
+    return CategoryService.instance;
+  }
+
+}
+
+export default CategoryService.getInstance();
