6238720cfe1be722bed69b8346a86028b52889bf
Mauricio Aguilera <mauricio@familify.com>
2024-08-28 17:38:13 -0500

    add expand icon

--- src/components/forms/fields/TextAreaField.tsx
+++ src/components/forms/fields/TextAreaField.tsx
@@ -2,16 +2,16 @@ import React, { ChangeEvent, useState } from 'react';
 
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { FormControl, IconButton, InputAdornment, TextField } from '@mui/material';
+import { FormControl, IconButton, InputAdornment, TextField, TextFieldProps } from '@mui/material';
 
-interface ITextAreaFieldProps {
+interface ITextAreaFieldProps extends Omit<TextFieldProps, 'onChange' | 'label'> {
   label: string;
   name: string;
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   fullWidth?: boolean;
-  minRows?: number;
   maxRows?: number;
+  minRows?: number;
 }
 
 const TextAreaField: React.FC<ITextAreaFieldProps> = ({
@@ -22,6 +22,7 @@ const TextAreaField: React.FC<ITextAreaFieldProps> = ({
   fullWidth = true,
   minRows = 1,
   maxRows = 6,
+  ...props
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
@@ -34,17 +35,21 @@ const TextAreaField: React.FC<ITextAreaFieldProps> = ({
       <TextField
         label={label}
         name={name}
-        multiline
         value={value}
         minRows={isExpanded ? maxRows : minRows}
         maxRows={maxRows}
         onChange={onChange}
+        multiline
+        {...props}
         InputProps={{
           endAdornment: (
             <InputAdornment position="end" sx={{ position: 'absolute', top: 14, right: 14 }}>
               <IconButton
                 sx={{
                   padding: 0,
+                  backdropFilter: 'blur(4px)',
+                  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                  borderRadius: '50%',
                   '&:focus': {
                     outline: 'none',
                   }
@@ -56,6 +61,7 @@ const TextAreaField: React.FC<ITextAreaFieldProps> = ({
               </IconButton>
             </InputAdornment>
           ),
+          ...props.InputProps,
         }}
       />
     </FormControl>
