6411bb00d915dbbbf41136d0fe7e1bdae15cf879
Mauricio Aguilera <mauricio@familify.com>
2024-08-28 19:21:14 -0500

    chnage importation app main

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import App from './App';
 import './index.css';
-import AppRoutes from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DndProvider backend={HTML5Backend}>
-        <AppRoutes />
-      </DndProvider>
-    </LocalizationProvider>
+    <App/>
   </React.StrictMode>,
 )
