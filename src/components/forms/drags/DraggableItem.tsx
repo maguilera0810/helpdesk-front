6f424fac6b0a2a4de1e1e74cf0224b43a4cdd598
Mauricio Aguilera <mauricio@familify.com>
2024-08-28 20:08:33 -0500

    drag item

--- src/components/forms/drags/DraggableItem.tsx
+++ src/components/forms/drags/DraggableItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import { Box, Paper } from '@mui/material';
-import { DragPreviewImage, useDrag } from 'react-dnd';
 
-
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 export interface Item {
   id: number;
   text: string;
@@ -12,44 +12,53 @@ export interface Item {
 
 interface DraggableItemProps {
   item: Item;
-  moveItem: (id: number, toRight: boolean) => void;
+  moveItem?: (id: number, toRight: boolean) => void;
 }
 
-const DraggableItem: React.FC<DraggableItemProps> = ({ item, moveItem }) => {
-  const [{ isDragging, opacity }, drag, preview] = useDrag(() => ({
-    type: 'ITEM',
-    item: { id: item.id },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-      opacity: monitor.isDragging() ? 0.4 : 1,
-    }),
-  }));
+const DraggableItem: React.FC<DraggableItemProps> = ({ item }) => {
 
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({
+    id: item.id
+  })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+
+  }
+
+  const isDragging = true;
   return (
-    <>
-      {item.img && <DragPreviewImage connect={preview} src={item.img} />}
-      <Paper
-        ref={drag}
-        sx={{
-          padding: 2,
-          marginBottom: 1,
-          backgroundColor: isDragging ? '#f0f0f0' : '#fff',
-          cursor: 'pointer',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        {item.img && (
-          <Box
-            component="img"
-            src={item.img}
-            alt={item.text}
-            sx={{ width: 32, height: 32, marginRight: 1 }}
-          />
-        )}
-        {item.text}
-      </Paper>
-    </>
+    <Paper
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      sx={{
+        padding: 2,
+        marginBottom: 1,
+        backgroundColor: isDragging ? '#f0f0f0' : '#fff',
+        cursor: 'pointer',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      {item.img && (
+        <Box
+          component="img"
+          src={item.img}
+          alt={item.text}
+          sx={{ width: 32, height: 32, marginRight: 1 }}
+        />
+      )}
+      {item.text} Hola
+    </Paper>
   );
 };
 
