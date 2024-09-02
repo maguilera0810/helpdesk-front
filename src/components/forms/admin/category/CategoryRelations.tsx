4c5a1b24beb79612ce8ab05129084c632329da41
Mauricio Aguilera <mauricio@familify.com>
2024-09-01 21:56:37 -0500

    category relations

---
+++ src/components/forms/admin/category/CategoryRelations.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+
+// import { Button, Paper } from '@mui/material';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+
+import { useCategory } from '../../../../hooks/useCategory';
+import useCategoryStore from '../../../../stores/useCategoryStore';
+
+
+
+
+const CategoryRelations: React.FC = () => {
+  const category = useCategoryStore((state) => state.category);
+  const {  updateCategory, fetchCategories } = useCategory();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpansion = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // const categoryId = parseInt(id);
+    // await updateCategory(categoryId, formData);
+    // navigate('/admin/users');
+  };
+
+  useEffect(() => {
+    category && fetchCategories({ "excl_id": category.id });
+    console.log(category);
+  }, [category]);
+
+  return (
+    <>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel1-header"
+        >
+          <Typography>Skills</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Type 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  )
+}
+
+export default CategoryRelations;
