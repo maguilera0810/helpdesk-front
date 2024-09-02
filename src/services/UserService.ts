b91550319cdf17312e12916bf0ff4f9a6799263d
Mauricio Aguilera <mauricio@familify.com>
2024-09-02 00:56:11 -0500

    delete comments

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -16,61 +16,6 @@ export class UserService extends BaseCrudService<User> {
     return UserService.instance;
   }
 
-  // TODO BORRAR CUANDO SE VERIFIQUE QUE FUNCIONEN LAS CLASES PADRE
-  // public async list(filters: { [key: string]: any } = {}): Promise<User[]> {
-  //   try {
-  //     const response = await this.axiosInstance.get<User[]>('', {
-  //       headers: this.getAuthHeaders(),
-  //       params: keysToSnake(filters),
-  //     });
-  //     return keysToCamel(response.data);
-  //   } catch (error) {
-  //     console.error('SERVICE: Error fetching users:', error);
-  //     throw error;
-  //   }
-  // }
-
-  // public async retrieve(userId: number): Promise<User> {
-  //   try {
-  //     const response = await this.axiosInstance.get<User>(`${userId}`,
-  //       { headers: this.getAuthHeaders() });
-  //     return keysToCamel(response.data);
-  //   } catch (error) {
-  //     console.error(`SERVICE: Error fetching user with id ${userId}:`, error);
-  //     throw error;
-  //   }
-  // }
-
-  // public async create(user: Partial<User>): Promise<User> {
-  //   try {
-  //     const response = await this.axiosInstance.post<User>('', keysToSnake(user),
-  //       { headers: this.getAuthHeaders() });
-  //     return keysToCamel(response.data);
-  //   } catch (error) {
-  //     console.error('SERVICE: Error creating user:', error);
-  //     throw error;
-  //   }
-  // }
-
-  // public async update(userId: number, user: Partial<User>): Promise<User> {
-  //   try {
-  //     const response = await this.axiosInstance.put<User>(`${userId}`, keysToSnake(user),
-  //       { headers: this.getAuthHeaders() });
-  //     return keysToCamel(response.data);
-  //   } catch (error) {
-  //     console.error(`SERVICE: Error updating user with id ${userId}:`, error);
-  //     throw error;
-  //   }
-  // }
-
-  // public async destroy(userId: number): Promise<void> {
-  //   try {
-  //     await this.axiosInstance.delete(`${userId}`, { headers: this.getAuthHeaders() });
-  //   } catch (error) {
-  //     console.error(`SERVICE: Error deleting user with id ${userId}:`, error);
-  //     throw error;
-  //   }
-  // }
 }
 
 export default UserService.getInstance();
