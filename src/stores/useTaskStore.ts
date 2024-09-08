4c8a1198fb0c72f86b3761de8275c4183d592fe2
Mauricio Aguilera <mauricio@familify.com>
2024-09-08 17:03:03 -0500

    task store

---
+++ src/stores/useTaskStore.ts
@@ -0,0 +1,25 @@
+import { produce } from 'immer';
+import { create, StateCreator } from 'zustand';
+import { ITaskState } from '../interfaces/StateInterfaces';
+
+
+const stateCreator: StateCreator<ITaskState, [], [], ITaskState> = (set) => ({
+  task: null,
+  tasks: [],
+  setTask: (task) =>
+    set(produce((state: ITaskState) => {
+      state.task = task;
+    })),
+  setTasks: (tasks) =>
+    set(produce((state: ITaskState) => {
+      state.tasks = tasks;
+    })),
+  clearTaks: () =>
+    set(produce((state: ITaskState) => {
+      state.tasks = [];
+    })),
+});
+
+const useCategoryStore = create<ITaskState>()(stateCreator);
+
+export default useCategoryStore;
