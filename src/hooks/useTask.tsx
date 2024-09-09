f150bb7f0dee4cf9ef837673743c25094eb065a3
Mauricio Aguilera <mauricio@familify.com>
2024-09-08 20:02:43 -0500

    task hook

--- src/hooks/useTask.tsx
+++ src/hooks/useTask.tsx
@@ -2,95 +2,109 @@ import { useCallback, useState } from 'react';
 import { Task } from '../interfaces/ModelInterfaces';
 import TaskService from '../services/TaskService';
 
+
 export const useTask = () => {
   const [task, setTask] = useState<Task | null>(null);
   const [tasks, setTasks] = useState<Task[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState<boolean>(false);
+  const [method, setMethod] = useState<"fetchTasks" | "fetchTask" | "createTask" | "updateTask" | "deleteTask" | null>(null);
+  const [success, setSuccess] = useState<boolean | null>(null);
 
   const fetchTasks = useCallback(async (filters: { [key: string]: any } = {}) => {
+    setMethod("fetchTasks");
     setLoading(true);
     setError(null);
-    setSuccess(false);
+    setSuccess(null);
     try {
       const taskList = await TaskService.list(filters);
       setTasks(taskList);
       setSuccess(true);
     } catch (error) {
+      setSuccess(false)
       console.error('Error fetching tasks:', error);
       setError('Error fetching tasks');
     } finally {
       setLoading(false);
+      setMethod(null);
     }
   }, []);
 
   const fetchTask = async (taskId: number) => {
+    setMethod("fetchTask");
     setLoading(true);
     setError(null);
-    setSuccess(false);
+    setSuccess(null);
     try {
       const task = await TaskService.retrieve(taskId);
       setTask(task);
       setSuccess(true);
       return task;
     } catch (error) {
+      setSuccess(false)
       console.error(`Error fetching task with id ${taskId}:`, error);
       setError('Error fetching task');
     } finally {
+      setMethod(null);
       setLoading(false);
     }
   };
 
   const createTask = async (task: Partial<Task>) => {
+    setMethod("createTask");
     setLoading(true);
     setError(null);
-    setSuccess(false);
+    setSuccess(null);
     try {
       const newTask = await TaskService.create(task);
-      setTasks((prevTasks) => [...prevTasks, newTask]);
+      setTask(newTask);
       setSuccess(true);
     } catch (error) {
+      setSuccess(false)
       console.error('Error creating task:', error);
       setError('Error creating task');
-      setSuccess(false);
+      setSuccess(null);
     } finally {
+      setMethod(null);
       setLoading(false);
     }
   };
 
   const updateTask = async (taskId: number, task: Partial<Task>) => {
+    setMethod("updateTask");
     setLoading(true);
     setError(null);
-    setSuccess(false);
+    setSuccess(null);
     try {
       const updatedTask = await TaskService.update(taskId, task);
-      setTasks((prevTasks) =>
-        prevTasks.map((u) => (u.id === taskId ? updatedTask : u))
-      );
+      setTask(updatedTask);
       setSuccess(true);
     } catch (error) {
+      setSuccess(false)
       console.error(`Error updating task with id ${taskId}:`, error);
       setError('Error updating task');
-      setSuccess(false);
+      setSuccess(null);
     } finally {
+      setMethod(null);
       setLoading(false);
     }
   };
 
   const deleteTask = async (taskId: number) => {
+    setMethod("deleteTask");
     setLoading(true);
     setError(null);
-    setSuccess(false);
+    setSuccess(null);
     try {
       await TaskService.destroy(taskId);
-      setTasks((prevTasks) => prevTasks.filter((u) => u.id !== taskId));
       setSuccess(true);
     } catch (error) {
+      setSuccess(false)
       console.error(`Error deleting task with id ${taskId}:`, error);
       setError('Error deleting task');
-      setSuccess(false);
+      setSuccess(null);
     } finally {
+      setMethod(null);
       setLoading(false);
     }
   };
@@ -101,6 +115,7 @@ export const useTask = () => {
     loading,
     error,
     success,
+    method,
     fetchTasks,
     fetchTask,
     createTask,
