e2a7159266f11afde8b239b03d291c82a6961b44
Mauricio Aguilera <mauricio@familify.com>
2024-09-14 17:42:10 -0500

    constants

---
+++ src/constants/formOptions.ts
@@ -0,0 +1,24 @@
+
+export const tasksTypeOptions = [
+  { value: 'preventive', label: 'Preventive' },
+  { value: 'emergency', label: 'Emergency' },
+];
+
+export const priorityOptions = [
+  { value: 'lowest', label: 'Lowest' },
+  { value: 'low', label: 'Low' },
+  { value: 'medium', label: 'Medium' },
+  { value: 'high', label: 'High' },
+  { value: 'highest', label: 'Highest' },
+];
+
+export const statusOptions = [
+  { value: 'to_do', label: 'Por hacer' },
+  { value: 'in_progress', label: 'En ejecución' },
+  { value: 'blocked', label: 'Bloqueado' },
+  { value: 'scheduled', label: 'Programado' },
+  { value: 'rescheduled', label: 'Reprogramado' },
+  { value: 'executed', label: 'Ejecutado' },
+  { value: 'to_validate', label: 'Por validar' },
+  { value: 'completed', label: 'Completado' },
+];
