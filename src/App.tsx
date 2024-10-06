89eb1993b63f1824353d2c9de36c7a98a4392ad9
Mauricio Aguilera <mauricio@familify.com>
2024-10-06 09:11:08 -0500

    i18n app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 import { I18nextProvider } from 'react-i18next';
-import './i18n';
 import i18n from './i18n';
 
 import AppRoutes from './routes';
