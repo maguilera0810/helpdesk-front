3482722babe37d9fc16604dd54dbd13dbab8e8ba
Mauricio Aguilera <mauricio@familify.com>
2024-10-06 23:01:57 -0500

    task Tracking

--- src/pages/support/task-tracking/TaskTracking.tsx
+++ src/pages/support/task-tracking/TaskTracking.tsx
@@ -1,13 +1,15 @@
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 
 import { Box, SelectChangeEvent, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import { useNavigate } from "react-router-dom";
 
 import MultipleSelectField from "../../../components/forms/fields/MultipleSelectField.tsx";
 import Layout from '../../../components/layouts/Layout.tsx';
+import { useUser } from "../../../hooks/useUser.tsx";
+import useTaskStore from "../../../stores/useTaskStore.ts";
 
 
 const gridSizes = {
@@ -17,24 +19,51 @@ const gridSizes = {
   xl: 3,
 };
 
+interface TaskTrackingFilter {
+  users: number[];
+  date: Dayjs | null;
+}
+
 const TaskTracking: FC = () => {
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState<object>({});
+  const [filters, setFilters] = useState<Partial<TaskTrackingFilter>>({ users: [], date: dayjs() });
+  // const {users} = useUserStore();
+  const { users, fetchUsers } = useUser()
+  const { currDate, setSchedule, setCurrDate } = useTaskStore()
+
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<string> | SelectChangeEvent<any[]>) => {
     const { name, value } = e.target;
-
     if (name) {
-      setFormData((prev: any) => ({
+      setFilters((prev) => ({
         ...prev,
         [name]: value
       }));
     }
   };
+
+  const handleDateChange = (newDate: Dayjs | null) => {
+    const cleanDate = newDate ? newDate.set('second', 0).set('millisecond', 0) : newDate;
+    setCurrDate(cleanDate);
+    setFilters((prev) => ({
+      ...prev,
+      ['date']: cleanDate
+    }));
+  }
   const handleSubmit = () => {
 
   }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    console.log(filters);
+  }, [filters]);
+
+
   return (
     <Layout>
       <Typography variant="h4" gutterBottom>
@@ -42,20 +71,25 @@ const TaskTracking: FC = () => {
       </Typography>
       <Box component="form" onSubmit={handleSubmit} sx={{ mt: 0 }}>
         <Grid container spacing={1} alignItems={"start"} alignContent={"start"} justifyContent={"start"}  >
-          <Grid size={gridSizes} key={"date"}>
+          <Grid size={{ ...gridSizes, sm: 2, md: 2, lg: 2, xl: 2 }} key={"date"}>
             <DatePicker
               label='Día'
+              name='date'
               sx={{ width: '100%' }}
-              defaultValue={dayjs()}
+              displayWeekNumber={true}
+              format={'DD/MM/YYYY'}
+              slotProps={{ textField: { fullWidth: true } }}
+              value={filters.date}
+              onChange={handleDateChange}
             />
           </Grid>
-          <Grid size={gridSizes} key={"personal"}>
+          <Grid size={"grow"} key={'users'}>
             <MultipleSelectField
-              value={[]}
+              value={filters.users ?? []}
               onChange={handleInputChange}
               label={'Personal'}
-              name={'personal'}
-              options={[]} />
+              name={'users'}
+              options={users.map(user => ({ value: user.id, label: `${user.firstName} ${user.lastName}` }))} />
           </Grid>
         </Grid>
       </Box>
