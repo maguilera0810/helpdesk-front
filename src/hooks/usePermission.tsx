4dae905a2a3f992fdefe613746664a814c77890a
Mauricio Aguilera <mauricio@familify.com>
2024-10-12 11:46:52 -0500

    role permission hooks

---
+++ src/hooks/usePermission.tsx
@@ -0,0 +1,118 @@
+import { useCallback, useState } from 'react';
+
+import { Permission } from '../interfaces/ModelInterfaces';
+import PermissionService from '../services/auth/PermissionService';
+
+export const usePermission = () => {
+  const [permission, setPermission] = useState<Permission | null>(null);
+  const [permissions, setPermissions] = useState<Permission[]>([]);
+  const [method, setMethod] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const fetchPermissions = useCallback(async (filters: { [key: string]: any } = {}) => {
+    setMethod('fetchPermissions');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const permissionList = await PermissionService.list(filters);
+      setPermissions(permissionList);
+      setSuccess(true);
+    } catch (error) {
+      console.error('Error fetching permissions:', error);
+      setError('Error fetching permissions');
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const fetchPermission = async (permissionId: number) => {
+    setMethod('fetchPermission');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const permission = await PermissionService.retrieve(permissionId);
+      setPermission(permission);
+      setSuccess(true);
+      return permission;
+    } catch (error) {
+      console.error(`Error fetching permission with id ${permissionId}:`, error);
+      setError('Error fetching permission');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createPermission = async (permission: Partial<Permission>) => {
+    setMethod('createPermission');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const newPermission = await PermissionService.create(permission);
+      setPermissions((prevPermissions) => [...prevPermissions, newPermission]);
+      setSuccess(true);
+    } catch (error) {
+      console.error('Error creating permission:', error);
+      setError('Error creating permission');
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updatePermission = async (permissionId: number, permission: Partial<Permission>) => {
+    setMethod('updatePermission');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const updatedPermission = await PermissionService.update(permissionId, permission);
+      setPermissions((prevPermissions) =>
+        prevPermissions.map((u) => (u.id === permissionId ? updatedPermission : u))
+      );
+      setSuccess(true);
+    } catch (error) {
+      console.error(`Error updating permission with id ${permissionId}:`, error);
+      setError('Error updating permission');
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deletePermission = async (permissionId: number) => {
+    setMethod('deletePermission');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      await PermissionService.destroy(permissionId);
+      setPermissions((prevPermissions) => prevPermissions.filter((u) => u.id !== permissionId));
+      setSuccess(true);
+    } catch (error) {
+      console.error(`Error deleting permission with id ${permissionId}:`, error);
+      setError('Error deleting permission');
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    permission,
+    permissions,
+    loading,
+    error,
+    success,
+    method,
+    fetchPermissions,
+    fetchPermission,
+    createPermission,
+    updatePermission,
+    deletePermission,
+  };
+};
---
+++ src/hooks/useRole.tsx
@@ -0,0 +1,118 @@
+import { useCallback, useState } from 'react';
+
+import { Role } from '../interfaces/ModelInterfaces';
+import RoleService from '../services/auth/RoleService';
+
+export const useRole = () => {
+  const [role, setRole] = useState<Role | null>(null);
+  const [roles, setRoles] = useState<Role[]>([]);
+  const [method, setMethod] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const fetchRoles = useCallback(async (filters: { [key: string]: any } = {}) => {
+    setMethod('fetchRoles');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const roleList = await RoleService.list(filters);
+      setRoles(roleList);
+      setSuccess(true);
+    } catch (error) {
+      console.error('Error fetching roles:', error);
+      setError('Error fetching roles');
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const fetchRole = async (roleId: number) => {
+    setMethod('fetchRole');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const role = await RoleService.retrieve(roleId);
+      setRole(role);
+      setSuccess(true);
+      return role;
+    } catch (error) {
+      console.error(`Error fetching role with id ${roleId}:`, error);
+      setError('Error fetching role');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createRole = async (role: Partial<Role>) => {
+    setMethod('createRole');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const newRole = await RoleService.create(role);
+      setRoles((prevRoles) => [...prevRoles, newRole]);
+      setSuccess(true);
+    } catch (error) {
+      console.error('Error creating role:', error);
+      setError('Error creating role');
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateRole = async (roleId: number, role: Partial<Role>) => {
+    setMethod('updateRole');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      const updatedRole = await RoleService.update(roleId, role);
+      setRoles((prevRoles) =>
+        prevRoles.map((u) => (u.id === roleId ? updatedRole : u))
+      );
+      setSuccess(true);
+    } catch (error) {
+      console.error(`Error updating role with id ${roleId}:`, error);
+      setError('Error updating role');
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteRole = async (roleId: number) => {
+    setMethod('deleteRole');
+    setLoading(true);
+    setError(null);
+    setSuccess(false);
+    try {
+      await RoleService.destroy(roleId);
+      setRoles((prevRoles) => prevRoles.filter((u) => u.id !== roleId));
+      setSuccess(true);
+    } catch (error) {
+      console.error(`Error deleting role with id ${roleId}:`, error);
+      setError('Error deleting role');
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    role,
+    roles,
+    loading,
+    error,
+    success,
+    method,
+    fetchRoles,
+    fetchRole,
+    createRole,
+    updateRole,
+    deleteRole,
+  };
+};
