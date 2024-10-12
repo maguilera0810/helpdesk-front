bb0617ea7c724c4d0b572d7e50435c047e0e1511
Mauricio Aguilera <mauricio@familify.com>
2024-10-12 13:01:50 -0500

    Role Table

--- src/components/tables/RoleTable.tsx
+++ src/components/tables/RoleTable.tsx
@@ -1,32 +1,51 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import { Box, Button } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { useNavigate } from 'react-router-dom';
 
+import { useRole } from '../../hooks/admin/useRole';
 
-const columns: GridColDef[] = [
+const COLUMNS: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'name', headerName: 'Name', width: 130 },
+  { field: 'title', headerName: 'Titulo', width: 130 },
   { field: 'code', headerName: 'Code', width: 130 },
 ];
+Object.freeze(COLUMNS);
 
 const RoleTable: FC = () => {
+  const navigate = useNavigate();
+  const { roles, loading, fetchRoles } = useRole();
+
+  useEffect(() => {
+    if (!loading) {
+      fetchRoles();
+    }
+  }, []);
+
+  const handleCreate = () => {
+    navigate('/admin/role/addNew/');
+  };
+
+  const handleUpdate = (id: number) => {
+    navigate(`/admin/role/${id}/`);
+  };
 
   return (
     <>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
-        <Button variant="contained" color="primary" href="/admin/users/create">
+        <Button variant="contained" color="primary" onClick={handleCreate}>
           Crear Rol
         </Button>
       </Box>
       <div style={{ height: 400, width: '100%' }}>
         <DataGrid
-          rows={[]}
-          columns={columns}
+          rows={roles}
+          columns={COLUMNS}
           loading={false}
           pageSizeOptions={[5, 10, 50, 100]}
           checkboxSelection
-          onRowClick={(params) => console.log(params.id as number)}
+          onRowClick={(params) => handleUpdate(params.id as number)}
         />
       </div>
     </>
