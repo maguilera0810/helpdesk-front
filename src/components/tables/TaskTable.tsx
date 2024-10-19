542cb5b296c68e4e01ff150f0d930bfb9971513c
Mauricio Aguilera <mauricio@familify.com>
2024-10-18 22:07:14 -0500

    task table

--- src/components/tables/TaskTable.tsx
+++ src/components/tables/TaskTable.tsx
@@ -1,10 +1,12 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Avatar, Box, Button, Typography } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router-dom';
 import { useTask } from '../../hooks/support/useTask';
+import useGlobalData from '../../hooks/useGlobalData';
 import useFilterStore from '../../stores/useFilterStore';
+import { User } from '../../interfaces/ModelInterfaces';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -21,8 +23,26 @@ Object.freeze(columns);
 
 const TaskTable: FC = () => {
   const navigate = useNavigate();
-  const { tasks, loading, error, fetchTasks, deleteTask } = useTask();
+  const { tasks, loading, error, fetchTasks } = useTask();
   const { filters, clearFilters } = useFilterStore();
+  const { lightUsers } = useGlobalData();
+
+
+  const getUserNameById = (userId: number) => {
+    return lightUsers.find(user => user.id === userId);
+  };
+
+  const rows = useMemo(() => {
+    return tasks.map(task => ({
+      ...task,
+      createdBy: getUserNameById(task.createdBy),
+      responsible: getUserNameById(task.responsible),
+    }));
+  }, [tasks, lightUsers]);
+
+  useEffect(() => {
+    return () => clearFilters();
+  }, [])
 
   useEffect(() => {
     if (!loading) {
@@ -38,6 +58,55 @@ const TaskTable: FC = () => {
     navigate(`/soporte/tareas/${id}/`);
   };
 
+  const handleUserProfile = (userId: number) => {
+    navigate(`/administracion/usuario/${userId}`);
+  };
+
+  // Función que renderiza el avatar y el nombre del usuario
+  const renderUserCell = (user?: Partial<User>, color: string = 'blue') => {
+    let text: string = '';
+    if (user?.firstName && user?.lastName) {
+      text = `${user.firstName.charAt(0)}${user.lastName.charAt(0)}`.toUpperCase();
+    }
+    const onclick = () => { user?.id && handleUserProfile(user.id); };
+    return (<Box
+      display="flex"
+      alignItems="center"
+      height="100%"
+      justifyContent="center"
+      alignContent={"center"}
+      sx={{ cursor: user ? 'pointer' : 'auto' }}
+      onClick={onclick}
+    >
+      <Avatar sx={{ bgcolor: color, marginRight: 2, maxWidth: 30, maxHeight: 30, fontSize: 14 }} >
+        {text}
+      </Avatar>
+    </Box>);
+  };
+  // Columnas con nombres de usuarios como enlaces
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'code', headerName: 'Código', width: 130 },
+    { field: 'title', headerName: 'Titulo', width: 130 },
+    { field: 'status', headerName: 'Estado', width: 130 },
+    { field: 'priority', headerName: 'Prioridad', width: 130 },
+    {
+      field: 'createdBy',
+      headerName: 'Creado por',
+      width: 130,
+      renderCell: (params) => renderUserCell(params.value, 'blue')
+    },
+    {
+      field: 'responsible',
+      headerName: 'Responsable',
+      width: 130,
+      renderCell: (params) => renderUserCell(params.value, 'blue')
+    },
+    { field: 'createdAt', headerName: 'Creado el', width: 130 },
+    { field: 'updatedAt', headerName: 'Actualizado el', width: 130 },
+  ];
+  Object.freeze(columns);
+
   return (
     <>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
@@ -48,7 +117,7 @@ const TaskTable: FC = () => {
       {error && <Typography color="error">{error}</Typography>}
       <div style={{ height: 400, width: '100%' }}>
         <DataGrid
-          rows={tasks}
+          rows={rows}
           columns={columns}
           loading={loading}
           pageSizeOptions={[5, 10, 50, 100]}
