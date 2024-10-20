9d923189dda958eaf82525f2938399acb967e9d9
Mauricio Aguilera <mauricio@familify.com>
2024-10-20 12:26:13 -0500

    task table

--- src/components/tables/TaskTable.tsx
+++ src/components/tables/TaskTable.tsx
@@ -1,16 +1,49 @@
 import { FC, useEffect, useMemo } from 'react';
 
-import { Avatar, Box, Button, Typography } from '@mui/material';
-import Chip from '@mui/material/Chip';
+import { Box, Button, Typography } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 import { useNavigate } from 'react-router-dom';
 import { useTaskStatus } from '../../hooks/settings/useTaskStatus';
 import { useTask } from '../../hooks/support/useTask';
 import useGlobalData from '../../hooks/useGlobalData';
-import { Priority, TaskStatus, User } from '../../interfaces/ModelInterfaces';
 import taskStatusStore from '../../stores/settings/taskStatusStore';
 import useFilterStore from '../../stores/useFilterStore';
+import { renderChipCell, renderUserCell } from './renders';
+
+const COLUMNS: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'code', headerName: 'Código', width: 130 },
+  { field: 'title', headerName: 'Titulo', width: 130 },
+  {
+    field: 'status',
+    headerName: 'Estado',
+    width: 130,
+    renderCell: (params) => renderChipCell(params.value)
+  },
+  {
+    field: 'priority',
+    headerName: 'Prioridad',
+    width: 130,
+    renderCell: (params) => renderChipCell(params.value)
+  },
+  {
+    field: 'createdBy',
+    headerName: 'Creado por',
+    width: 130,
+    renderCell: (params) => renderUserCell(params.value, 'blue')
+  },
+  {
+    field: 'responsible',
+    headerName: 'Responsable',
+    width: 130,
+    renderCell: (params) => renderUserCell(params.value, 'blue')
+  },
+  { field: 'createdAt', headerName: 'Creado el', width: 130 },
+  { field: 'updatedAt', headerName: 'Actualizado el', width: 130 },
+];
+Object.freeze(COLUMNS);
+
 
 const TaskTable: FC = () => {
   const navigate = useNavigate();
@@ -64,89 +97,6 @@ const TaskTable: FC = () => {
     navigate(`/soporte/tareas/${id}/`);
   };
 
-  const handleUserProfile = (userId: number) => {
-    navigate(`/administracion/usuario/${userId}`);
-  };
-
-  // Función que renderiza el avatar y el nombre del usuario
-  const renderUserCell = (user?: Partial<User>, color: string = 'blue') => {
-    let text: string = '';
-    if (user?.firstName && user?.lastName) {
-      text = `${user.firstName.charAt(0)}${user.lastName.charAt(0)}`.toUpperCase();
-    }
-    const onclick = () => { user?.id && handleUserProfile(user.id); };
-    return (
-      <Box
-        display="flex"
-        alignItems="center"
-        height="100%"
-        justifyContent="center"
-        alignContent={"center"}
-        sx={{ cursor: user ? 'pointer' : 'auto' }}
-        onClick={onclick}
-      >
-        <Avatar sx={{ bgcolor: color, marginRight: 2, maxWidth: 30, maxHeight: 30, fontSize: 14 }} >
-          {text}
-        </Avatar>
-      </Box>
-    );
-  };
-  const renderChipCell = (elem: Priority | TaskStatus) => {
-    let bgcolor: string = 'lightgray'
-    let title: string = 'N/A'
-    if (elem) {
-      bgcolor = elem.color ?? 'lightgray';
-      title = elem.title;
-    }
-    return (
-      <Chip
-        label={title}
-        sx={{
-          bgcolor,
-          color: '#FFF',
-          height: '1.5rem',
-          '& .MuiChip-label': {
-            fontSize: 12,
-            fontWeight: 'bold',
-          },
-        }}
-      />
-    );
-  };
-  // Columnas con nombres de usuarios como enlaces
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'code', headerName: 'Código', width: 130 },
-    { field: 'title', headerName: 'Titulo', width: 130 },
-    {
-      field: 'status',
-      headerName: 'Estado',
-      width: 130,
-      renderCell: (params) => renderChipCell(params.value)
-    },
-    {
-      field: 'priority',
-      headerName: 'Prioridad',
-      width: 130,
-      renderCell: (params) => renderChipCell(params.value)
-    },
-    {
-      field: 'createdBy',
-      headerName: 'Creado por',
-      width: 130,
-      renderCell: (params) => renderUserCell(params.value, 'blue')
-    },
-    {
-      field: 'responsible',
-      headerName: 'Responsable',
-      width: 130,
-      renderCell: (params) => renderUserCell(params.value, 'blue')
-    },
-    { field: 'createdAt', headerName: 'Creado el', width: 130 },
-    { field: 'updatedAt', headerName: 'Actualizado el', width: 130 },
-  ];
-  Object.freeze(columns);
-
   return (
     <>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
@@ -158,7 +108,7 @@ const TaskTable: FC = () => {
       <div style={{ minHeight: 400, width: '100%' }}>
         <DataGrid
           rows={rows}
-          columns={columns}
+          columns={COLUMNS}
           loading={loading}
           pageSizeOptions={[5, 10, 50, 100]}
           checkboxSelection
