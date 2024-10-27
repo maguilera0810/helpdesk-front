07293a30e38541111afc6407992ae0b992030362
Mauricio Aguilera <mauricio@familify.com>
2024-10-27 17:18:41 -0500

    issue coment store

--- src/stores/support/issueCommentStore.ts
+++ src/stores/support/issueCommentStore.ts
@@ -5,8 +5,13 @@ import storeCreator from '../core/storeCreator';
 
 
 const stateCreator: StateCreator<IssueCommentState, [], [], IssueCommentState> = (set) => ({
+  issue: undefined,
   issueComment: undefined,
   issueComments: [],
+  setIssue: (issue) =>
+    set(produce((state: IssueCommentState) => {
+      state.issue = issue;
+    })),
   setIssueComment: (issueComment) =>
     set(produce((state: IssueCommentState) => {
       state.issueComment = issueComment;
@@ -18,6 +23,7 @@ const stateCreator: StateCreator<IssueCommentState, [], [], IssueCommentState> =
 
   clearState: () =>
     set(produce((state: IssueCommentState) => {
+      state.issue = undefined;
       state.issueComment = undefined;
       state.issueComments = [];
     })),
