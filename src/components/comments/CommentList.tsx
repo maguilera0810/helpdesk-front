c56d5a1bcb1960f9ed1b659bd769f4489e8bb4ab
Mauricio Aguilera <mauricio@familify.com>
2024-10-27 18:07:04 -0500

    comment list

--- src/components/comments/CommentList.tsx
+++ src/components/comments/CommentList.tsx
@@ -1,42 +1,66 @@
 import { FC, useEffect, useState } from 'react';
 
-import { Box, List, Paper, Typography } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { Box, IconButton, List, Paper, Typography } from '@mui/material';
+import { ComentListProps } from '../../interfaces/ComponentInterfaces';
 import { IssueComment, TaskComment } from '../../interfaces/ModelInterfaces';
 import issueCommentStore from '../../stores/support/issueCommentStore';
 import taskCommentStore from '../../stores/support/taskCommentStore';
+import CommentFormDialog from '../forms/comments/CommentFormDialog';
 import Comment from './Comment';
 
-interface ComentListProps {
-  type: 'issue' | 'task';
-}
+const CommentList: FC<ComentListProps> = ({ type, onSave }) => {
+  const { taskComment, taskComments } = taskCommentStore();
+  const { issueComment, issueComments } = issueCommentStore();
 
-const CommentList: FC<ComentListProps> = ({ type }) => {
-  const { taskComments } = taskCommentStore();
-  const { issueComments } = issueCommentStore();
   const [comments, setComments] = useState<(IssueComment | TaskComment)[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
-    if (type === 'task') {
-      setComments(taskComments);
-    }
+    type === 'task' && setComments(taskComments);
   }, [type, taskComments]);
 
   useEffect(() => {
-    if (type === 'issue') {
-      setComments(issueComments);
-    }
+    type === 'issue' && setComments(issueComments);
   }, [type, issueComments]);
+
 
   return (
     <Box>
-      <Typography variant="h4" gutterBottom>
-        Comentarios
-      </Typography>
+      <Box display="flex" alignItems="center" justifyContent="flex-start" mb={2} gap={1}>
+        <Typography variant="h4" >
+          Comentarios:
+        </Typography>
+        <IconButton color="primary" onClick={handleOpen}>
+          <Add />
+        </IconButton>
+      </Box>
       <Paper elevation={3} sx={{ padding: 2, backgroundColor: 'background.paper' }}>
-        <List>
+        {comments.length ? (<List>
           {comments.map((c) => <Comment key={c.id} comment={c} />)}
-        </List>
+        </List>) : (
+          <Typography variant="body1" color="text.secondary" align="left">
+            No hay comentarios
+          </Typography>
+        )
+        }
       </Paper>
+      <CommentFormDialog
+        open={open}
+        type={type}
+        issueComment={issueComment}
+        taskComment={taskComment}
+        onClose={handleClose}
+        onConfirm={onSave}
+      />
     </Box>
   );
 };
