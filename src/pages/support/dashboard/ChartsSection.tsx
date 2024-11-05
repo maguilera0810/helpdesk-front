6a5cddd47953c7252805a499e209d14f8cf97d1c
Mauricio Aguilera <mauricio@familify.com>
2024-11-05 09:25:38 -0500

    Chart Section

--- src/pages/support/dashboard/ChartsSection.tsx
+++ src/pages/support/dashboard/ChartsSection.tsx
@@ -5,7 +5,7 @@ import StatusTasksBarChart from '../../../components/data_visualization/StatusTa
 import TaskCategoriesPieChart from '../../../components/data_visualization/TaskCategoriesPieChart';
 import Swipper from '../../../components/layouts/Swipper';
 
-type typeChart = 'statusTasks' | 'IssueTask' | 'TaskCategories'
+type typeChart = 'statusTasks' | 'IssueTask' | 'TaskCategories' | 'IssueCategories';
 
 const RenderChart = (type: typeChart) => {
   if (type === 'statusTasks') {
@@ -14,13 +14,15 @@ const RenderChart = (type: typeChart) => {
     return <IssueTaskLineChart key={type} />
   } else if (type === 'TaskCategories') {
     return <TaskCategoriesPieChart key={type} />
+  } else if (type === 'IssueCategories') {
+    return <TaskCategoriesPieChart key={type} />
   }
   return <></>
 }
 
 const ChartsSection: FC = () => {
 
-  const charts: typeChart[] = ['TaskCategories', 'statusTasks', 'IssueTask']
+  const charts: typeChart[] = ['TaskCategories', 'IssueCategories', 'statusTasks', 'IssueTask']
   return (
     <Swipper movementStep={1.5}>
       {charts.map((chart) => RenderChart(chart))}
